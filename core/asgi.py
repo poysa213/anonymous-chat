9859dc7069a60702d7c8e326e9c6444614933c00
poysa213 <hanaiayoucef@gmail.com>
2023-06-10 19:57:06 +0100

    inital funcional chat

---
+++ core/asgi.py
@@ -0,0 +1,22 @@
+import os
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
+
+django_asgi_app = get_asgi_application()
+
+import chat.routing
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
+        ),
+    }
+)
