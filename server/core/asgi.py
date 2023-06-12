64e5e9f5d496191a3a0e774e98d4fc33f7030e39
poysa213 <hanaiayoucef@gmail.com>
2023-06-12 19:37:17 +0100

    fix typos

--- server/core/asgi.py
+++ server/core/asgi.py
@@ -5,7 +5,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 
 django_asgi_app = get_asgi_application()
