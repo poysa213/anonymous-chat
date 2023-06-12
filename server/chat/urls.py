e67b59e0afaa9f2ec0fa58bd3235ed0d4004de93
poysa213 <hanaiayoucef@gmail.com>
2023-06-13 00:31:42 +0100

    add health-check route

--- server/chat/urls.py
+++ server/chat/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("helth", views.healthCheck, name="helth"),
     path("<str:room_name>/", views.room, name="room"),
 ]
--- server/chat/views.py
+++ server/chat/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 
 
@@ -11,3 +11,9 @@ def index(request):
 
 def room(request, room_name):
     return render(request, "chat/room.html", {"room_name": room_name})
+
+
+
+
+def healthCheck(request):
+    return HttpResponse(status=200)
